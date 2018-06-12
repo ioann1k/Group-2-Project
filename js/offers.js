571504bca873950864d637c6405b780bcab92132
Ioan Nikolov <ioann1k@abv.bg>
2018-06-12 05:29:03 +0200

    Added scrolling functionality and extended the dimensions of the layout

--- js/offers.js
+++ js/offers.js
@@ -1,3 +1,5 @@
+let PLANET_THROTTLE_MOVEMENT = 30;
+
 $(document).ready(function () {
 	var throttleScroll = _.throttle(function (event) {
 		floatOnScroll(event);
@@ -7,5 +9,18 @@ $(document).ready(function () {
 });
 
 function floatOnScroll (event) {
-	console.log(event);
+	let deltaY = event.originalEvent.deltaY;
+	let planetTransform = $('.planet').css('transform');
+	planetTransform = planetTransform.split(/[()]/)[1].split(',');
+
+	let planetTranslateY = parseInt(planetTransform[planetTransform.length - 1]);
+	// if (planetTranslateY >= -(PLANET_THROTTLE_MOVEMENT * 2) && planetTranslateY <= (PLANET_THROTTLE_MOVEMENT * 2)) {
+		if (deltaY > 0) {
+			planetTranslateY += PLANET_THROTTLE_MOVEMENT;
+			$('.planet').css('transform', 'translateY(' + planetTranslateY + 'px)');
+		} else {
+			planetTranslateY -= PLANET_THROTTLE_MOVEMENT;
+			$('.planet').css('transform', 'translateY(' + planetTranslateY + 'px)');
+		}
+	// }
 }
